4ed790c1947840ff658aef4d6d6993beed102216
Fredrik Adolfsson <fread50@lille27.ida.liu.se>
2020-03-11 21:21:16 +0100

    hej

--- static/styles/client.js
+++ static/styles/client.js
@@ -128,6 +128,9 @@ function create_web_socket(token)
     // ws = new WebSocket("wss://" + document.domain + ":5000/create_socket");
    // ws = new WebSocket("ws://" + location.host + "/create_socket");
     //ws = new WebSocket("wss://" + location.host + "/create_socket");
+    console.log(location);
+    console.log(location.host);
+    console.log(location.port);
     console.log("wss://" + document.domain + location.port + "/create_socket");
     ws = new WebSocket("wss://" + document.domain + location.port + "/create_socket");
     ws = new WebSocket("ws://" + document.domain + location.port + "/create_socket");
